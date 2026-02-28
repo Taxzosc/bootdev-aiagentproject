ea98c1cf6872b360b4b09b5b0244192509949a7c
Taxzosc <martinfh9@gmail.com>
2026-02-28 09:13:35 +0100

    made a function to read the info of a file

---
+++ functions/get_files_info.py
@@ -0,0 +1,27 @@
+import os
+
+
+def get_file_info(working_directory, directory="."):
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs #returns true/false.
+        if valid_target_dir == False:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        
+        if os.path.isdir(target_dir) == False: #"if the directory arguement is not a directory, return an error string"
+            return f'Error: "{directory}" is not a directory'
+        
+        file_info = ""
+        for item in os.listdir(target_dir):
+            file_path = os.path.join(target_dir, item)
+            file_size = os.path.getsize(file_path)
+            is_dir = os.path.isdir(file_path)
+            file_info += "\n"
+            file_info += f"- {item}: file_size={file_size} bytes, is_dir={is_dir}"
+        return file_info
+    except Exception as e:
+        print(f"Error: {e}")
+
+        #DIFFERENCE BETWEEN string += string and append to list then "\n".join(list) is massive in terms of
+        #performance. check lesson boots chat or own documentation for more info.
---
+++ test_get_files_info.py
@@ -0,0 +1,6 @@
+from functions.get_files_info import get_file_info
+
+print(get_file_info("calculator","."))
+print(get_file_info("calculator", "pkg"))
+print(get_file_info("calculator", "/bin"))
+print(get_file_info("calculator", "../"))
